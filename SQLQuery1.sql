36cff43063d62eb0b84c61efb86c6f9b5fa43371
Haofangliu <hliu0029@student.monash.edu>
2019-11-14 16:26:21 +1100

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [dbo].[Location] ([Name], [Description], [Latitude], [Longitude])
+VALUES (N'Monash University', N'Caulfield Campus', CAST(-37.87682300 AS
+Decimal(10, 8)), CAST(145.04583700 AS Decimal(11, 8)))
+INSERT INTO [dbo].[Location] ([Name], [Description], [Latitude], [Longitude])
+VALUES (N'Monash University', N'Clayton Campus', CAST(-37.91500000 AS
+Decimal(10, 8)), CAST(145.13000000 AS Decimal(11, 8)))
